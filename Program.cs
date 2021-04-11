6df955d2b369000b69b46653be3cedc72c3c6e84
MstrssZ <superoranges14@gmail.com>
2021-04-11 19:18:35 -0400

    added to credits

--- Program.cs
+++ Program.cs
@@ -196,7 +196,10 @@ namespace Text_Adventure
         public static void Begin()
         {
             Console.Clear();
-            Console.Write("This game was made by me. \n published to git at https://github.com/MstrssZ/game-learn-2d-scroll.git");
+            Console.Write("This game was made by me. \nPublished to git at https://github.com/MstrssZ/game-learn-2d-scroll.git \n" +
+                            "Feel free to branch and work on it on your own, or to add issues for more features.\n" +
+                            "have fun");
+            Console.ReadKey(true);
         }
     }
 }
