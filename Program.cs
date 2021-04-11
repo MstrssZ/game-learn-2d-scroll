afd92af21d6ff081bf9e785491bada13784103dd
MstrssZ <superoranges14@gmail.com>
2021-04-11 17:26:19 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Text_Adventure
+{
+    class Program
+    {
+        // vars for character info display
+        public static int Character_HP_Current { get; set; }
+        public static int Character_HP_Full { get; set; }
+        public static int Character_EXP_Current { get; set; }
+        public static int Character_EXP_Full { get; set; }
+        public static int Character_LVL { get; set; }
+ 
+        public static string Character_Name { get; set; }
+
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Text Adventure 0.0";
+            Console.WriteLine("Welcome to Text Adventure 0.0");
+            Console.WriteLine("Press a key to continue");
+
+            Console.ReadKey(true);
+            Console.Clear();
+
+            Console.Write
+                (
+                "You awake to find yourself in an empty room without and noticable way for you to have entered\n" +
+                "Upon trying to remember how you got here a sharp throb in you spine causes you to ...\n" +
+                "...\n" +
+                "....\n" +
+                "...\n" +
+                "...\n" +
+                "expo\n"
+                );
+            Console.ReadKey(true);
+            Console.Clear();
+
+            Console.Write("What would you like to do?\n" +
+                "1. Play\n" +
+                "2. Options (not implemented yet)\n" +
+                "3. Credits\n" +
+                "4. Exit\n");
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.D1:
+                    GameStart();
+                    break;
+
+                case ConsoleKey.D2:
+                    break;
+
+                case ConsoleKey.D3:
+                    break;
+
+                case ConsoleKey.D4:
+                    break;
+
+                default:
+                    Console.WriteLine("not 1");
+                    break;
+            }
+        }
+        public static void GameStart() //draws GUI character info display an enters game loop
+        {
+            Console.Clear();
+            
+            //intitalize vars for character info display
+            Character_HP_Current = 20;
+            Character_HP_Full = 100;
+            Character_EXP_Current = 10;
+            Character_EXP_Full = 50;
+            Character_LVL = 1;
+            Character_Name = "name of character";
+
+
+
+            for (int i = 0; i < 79; i++)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.WriteLine("~");
+                Console.SetCursorPosition(i, 2);
+                Console.WriteLine("~");
+            }
+            //draw name
+            Console.SetCursorPosition(0, 1);
+            Console.WriteLine(Character_Name);
+
+            //draw HP
+            Console.SetCursorPosition(25, 1);
+            Console.WriteLine("HP: {0}/{1}", Character_HP_Current, Character_HP_Full);
+
+            //draw EXP
+            Console.SetCursorPosition(40, 1);
+            Console.WriteLine("EXP: {0}/{1}", Character_EXP_Current, Character_EXP_Full);
+
+            //draw LVL
+            Console.SetCursorPosition(60, 1);
+            Console.WriteLine("LVL: {0}", Character_LVL);
+
+            //move cursor to top left
+            Console.SetCursorPosition(0, 0);
+
+            //start game loop
+            GameLoop();
+
+        }
+        static void GameLoop()
+        {
+            int Character_Pos_LR = 10;
+            int Character_Pos_UD = 10;
+            bool GameOver = false;
+
+
+
+            Console.SetCursorPosition(Character_Pos_LR, Character_Pos_UD);
+            Console.WriteLine("X");
+            do
+            {
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        //remove current character
+                        Console.SetCursorPosition(Character_Pos_LR, Character_Pos_UD);
+                        Console.WriteLine(" ");
+
+                        //move character up one
+                        Character_Pos_UD = Character_Pos_UD - 1;
+                        Console.SetCursorPosition(Character_Pos_LR, Character_Pos_UD);
+                        Console.WriteLine("X");
+                        Console.SetCursorPosition(0, 0);
+
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        //remove current character
+                        Console.SetCursorPosition(Character_Pos_LR, Character_Pos_UD);
+                        Console.WriteLine(" ");
+
+                        //move character up one
+                        Character_Pos_UD = Character_Pos_UD + 1;
+                        Console.SetCursorPosition(Character_Pos_LR, Character_Pos_UD);
+                        Console.WriteLine("X");
+                        Console.SetCursorPosition(0, 0);
+
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        //remove current character
+                        Console.SetCursorPosition(Character_Pos_LR, Character_Pos_UD);
+                        Console.WriteLine(" ");
+
+                        //move character up one
+                        Character_Pos_LR = Character_Pos_LR - 1;
+                        Console.SetCursorPosition(Character_Pos_LR, Character_Pos_UD);
+                        Console.WriteLine("X");
+                        Console.SetCursorPosition(0, 0);
+
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        //remove current character
+                        Console.SetCursorPosition(Character_Pos_LR, Character_Pos_UD);
+                        Console.WriteLine(" ");
+
+                        //move character up one
+                        Character_Pos_LR = Character_Pos_LR + 1;
+                        Console.SetCursorPosition(Character_Pos_LR, Character_Pos_UD);
+                        Console.WriteLine("X");
+                        Console.SetCursorPosition(0, 0);
+                        break;
+                }
+            } while (!GameOver);
+        }
+        
+    }
+}
---
+++ Render.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Text_Adventure
+{
+    class Render
+    {
+        static void ScreenUpdate(string refreshPiece)
+        {
+            switch (refreshPiece)
+            {
+                case "Character_HP":
+                    //draw HP
+                    Console.SetCursorPosition(25, 1);
+                    Console.WriteLine("HP: {0}/{1}", Program.Character_HP_Current, Program.Character_HP_Full);
+                    break;
+                case "Character_EXP":
+                    //draw EXP
+                    Console.SetCursorPosition(40, 1);
+                    Console.WriteLine("EXP: {0}/{1}", Program.Character_EXP_Current, Program.Character_EXP_Full);
+                    break;
+                case "Character_LVL":
+                    //draw LVL
+                    Console.SetCursorPosition(60, 1);
+                    Console.WriteLine("LVL: {0}", Program.Character_LVL);
+
+
+
+
+
+                    /*
+                     on LVL up:
+                        max HP +10
+                        max EXP *1.2
+                        heal for 10% of new max
+                        reset EXP = EXP-MaxEXP 
+                        
+                     */
+
+
+
+                    break;
+                default:
+                    break;
+            }
+
+
+
+            return;
+        }
+    }
+}
